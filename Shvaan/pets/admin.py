65a9bc3e7fd2a01fe8fc0380617e8d7a5f21d251
Prashant9683 <amenu4elc21048@am.students.amrita.edu>
2022-11-26 15:15:45 +0530

    added models for pets

--- Shvaan/pets/admin.py
+++ Shvaan/pets/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Pet
+
+class PetAdmin(admin.ModelAdmin):
+    list_display = ['name', 'type_of_pet', 'species', 'breed', 'age']
+    list_filter = ['type_of_pet', 'species', 'breed']
+    search_fields = ['name', 'type_of_pet', 'species', 'breed']
+admin.site.register(Pet, PetAdmin)
--- Shvaan/pets/models.py
+++ Shvaan/pets/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from user.models import User
 
 # Create your models here.
+class Pet(models.Model):
+    current_owner_name = models.CharField(max_length=50, default='', blank=True, verbose_name='Current Owner Name')
+    name = models.CharField(max_length=50)
+    TYPES = (
+        ('Dog', 'Dog'),
+        ('Cat', 'Cat'),
+        ('Hamster', 'Hamster'),
+        ('Other', 'Other'),
+    )
+    type_of_pet = models.CharField(choices=TYPES, max_length=50, default='Dog',verbose_name='Type of Pet')
+    species = models.CharField(max_length=50)
+    breed = models.CharField(max_length=50)
+    description = models.TextField()
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.name
